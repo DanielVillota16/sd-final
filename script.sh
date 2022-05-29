dda3467070dde5b56312eb42f143ba14c9c48e51
DanielVillota16 <danielvillota1016@gmail.com>
2022-05-28 21:12:40 -0500

    script updated

--- script.sh
+++ script.sh
@@ -1,12 +1,15 @@
-#!bin/bash
+#!/bin/bash
 
 minikube start
-minikube addons enable ingress
-
-helm install my-redis -f ./redis/values.yaml bitnami/redis
-helm install final-project ./final-project
+helm install my-redis -f ./redis/values.yaml bitnami/redis > /dev/null
+helm install final-project ./final-project > /dev/null
+minikube addons enable ingress > /dev/null
+echo
+echo "Wait 60 seconds till everything gets up and ready..."
+sleep 60
 
 # SMOKE TEST
+
 curl http://$(minikube ip)/highest
 curl http://$(minikube ip)/updatehighest/5142
 curl http://$(minikube ip)/highest
